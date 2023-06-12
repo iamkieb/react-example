f280964d03e210d773ee846723557f4c16ec1651
Kieren Bennett <kie@whoiskie.com>
2023-06-12 17:18:07 +0100

    stack navigator

---
+++ Stack-demo1.js
@@ -0,0 +1,76 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { StatusBar } from 'expo-status-bar'
+import { useState } from 'react'
+import { StyleSheet, Text, View, Pressable } from 'react-native'
+// import { SafeAreaView } from 'react-native-safe-area-context';
+// import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+const Stack = createStackNavigator()
+
+function ScreenA({ navigation }) {
+  const onPressHandler = () => {
+    navigation.navigate('Screen_B')
+  }
+
+  return (
+    <View style={styles.body}>
+      <Text style={styles.text}>Screen A</Text>
+      <Pressable
+        onPress={onPressHandler}
+        style={({ pressed }) => ({
+          backgroundColor: pressed ? '#ddd' : '#0f0',
+        })}
+      >
+        <Text style={styles.text}>Go to Screen B</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+function ScreenB({ navigation }) {
+  const onPressHandler = () => {
+    // navigation.navigate('Screen_A');
+    navigation.goBack()
+  }
+
+  return (
+    <View style={styles.body}>
+      <Text style={styles.text}>Screen B</Text>
+      <Pressable
+        onPress={onPressHandler}
+        style={({ pressed }) => ({
+          backgroundColor: pressed ? '#ddd' : '#0f0',
+        })}
+      >
+        <Text style={styles.text}>Go to Screen A</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Screen_A" component={ScreenA} />
+        <Stack.Screen name="Screen_B" component={ScreenB} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    margin: 10,
+  },
+})
+
+export default App
